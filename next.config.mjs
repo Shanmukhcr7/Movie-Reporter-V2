86524f497cf980e7a909895dca9719229286e59b
VEEREPALLI SHANMUKH <shanmukhchowdary63@gmail.com>
2026-01-04 18:43:33 +0530

    Configure for Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,22 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.moviereporter.in',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
+  },
 }
 
 export default nextConfig
